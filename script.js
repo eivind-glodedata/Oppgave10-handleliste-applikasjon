3f04430bc94621e2c38b26dfd1887cc620a3177b
eivind-glodedata <eivind.glodedata@gmail.com>
2025-06-18 13:54:46 +0200

    ferdig oppgave

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const shoppingList = [];
+const shoppingList = ["melk", "brød", "smør", "øl", "egg", "appelsin"];
 const shoppingListEntry = document.getElementById("shoppingListEntry");
 const showShoppingList = document.getElementById("showShoppingList");
 const pushButton = document.getElementById("pushButton");
@@ -8,37 +8,9 @@ const shiftButton = document.getElementById("shiftButton");
 const emptyButton = document.getElementById("emptyButton");
 const sortButton = document.getElementById("sortButton");
 
-console.log(shoppingList);
-shoppingList.push("milk", "bread", "beer");
-console.log(shoppingList)
-console.log(shoppingList[1]);
-console.log(shoppingList.toString())
-console.log(shoppingList.join(", "))
+showShoppingList.innerText = shoppingList.join(", ");
 
-showShoppingList.innerText = shoppingList.join("--");
-
-let removedShoppingItem = shoppingList.pop(); //fjerner siste element
-console.log(removedShoppingItem);
-console.log(shoppingList.join(", "));
-shoppingList.push("beef");
-console.log(shoppingList.join(", "));
-
-showShoppingList.innerText = shoppingList.join("--");
-
-shoppingList.shift(); //fjerner første element
-console.log(shoppingList.join("-"))
-
-showShoppingList.innerText = shoppingList.join("--");
-
-shoppingList.unshift("butter"); // legg til element i starten
-console.log(shoppingList.join(", "));
-shoppingList.pop()
-shoppingList.pop()
-shoppingList.pop()
-console.log(shoppingList.toString())
-console.log(shoppingList)
-
-if (shoppingList.toString() === ""){showShoppingList.innerText = "Listen er tom"};
+if (shoppingList.toString() === ""){showShoppingList.innerText = "Listen er tom."};
 
 pushButton.addEventListener("click", pushItem);
 popButton.addEventListener("click", popItem);
@@ -49,17 +21,16 @@ sortButton.addEventListener("click", sortList);
 
 function pushItem() { //legger til på slutten av arrayet
     console.log("Pushknappen virker");
-    console.log(shoppingListEntry.value);
+    if(shoppingListEntry.value.trim() !== ""){ //bare hvis noe er skrevet inn
     shoppingList.push(shoppingListEntry.value);
     showShoppingList.innerText = shoppingList.join(", ");
-    shoppingListEntry.value = "";
-    if (shoppingList.toString() == "" || shoppingList.every(item => item === '')){showShoppingList.innerText = "Listen er tom"};
+    shoppingListEntry.value = "";};
 };
 
 function popItem() {
     shoppingList.pop();
     showShoppingList.innerText = shoppingList.join(", ");
-    if (shoppingList.toString() == "" || shoppingList.every(item => item === "")){showShoppingList.innerText = "Listen er tom"};
+    if (shoppingList.every(item => item === "")){showShoppingList.innerText = "Listen er tom."};
 };
 
 function unshiftItem() { //legger til i starten, til venstre
@@ -67,7 +38,7 @@ function unshiftItem() { //legger til i starten, til venstre
     shoppingList.unshift(shoppingListEntry.value);
     showShoppingList.innerText = shoppingList.join(", ");
     shoppingListEntry.value = "";
-    if (shoppingList.every(item => item === "")) showShoppingList.innerText = "Listen er tom";
+    if (shoppingList.every(item => item === "")) showShoppingList.innerText = "Listen er tom.";
 };
 
 function shiftItem () { //fjerner første element
@@ -76,7 +47,7 @@ function shiftItem () { //fjerner første element
     showShoppingList.innerText = shoppingList.join(", ");
     shoppingListEntry.value = "";
     if (shoppingList.every(item => item === "")) {
-        showShoppingList.innerText = "Listen er tom";
+        showShoppingList.innerText = "Listen er tom.";
         //shoppingList = []; funker ikke til å tømme tomrom??? sikkert fordi det bare kommer et nytt array inni funksjonen
         shoppingList.splice(0, shoppingList.length); // funker heller ikke? fordi returnerer også en kopi
         shoppingList.length = 0; //eneste som funker! for det skriver til opprinnelig array
@@ -85,7 +56,7 @@ function shiftItem () { //fjerner første element
 
 function emptyList () {
     shoppingList.length = 0;
-    showShoppingList.innerText = "Listen er tom";
+    showShoppingList.innerText = "Listen er tom.";
 }
 
 function sortList () {
@@ -93,11 +64,6 @@ function sortList () {
     showShoppingList.innerText = shoppingList.join(", ");
     if (shoppingList.every(item => item === "")) {
         shoppingList.length = 0;
-        showShoppingList.innerText = "Listen er tom";
+        showShoppingList.innerText = "Listen er tom.";
 };
 };
-
-let text = "Hello world, welcome to the universe.";
-let result = text.includes("olrld");
-console.log(result);
-
